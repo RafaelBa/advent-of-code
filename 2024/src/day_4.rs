d541df2ff00f802bed9070478ba5583b156f6caa
Rafael <11751298+RafaelBa@users.noreply.github.com>
2024-12-04 22:32:19 +0100

    day 4 - part 1 - working on test input

--- 2024/src/day_4.rs
+++ 2024/src/day_4.rs
@@ -1,4 +1,5 @@
 pub fn solve_1() -> u32 {
+    count_xmas(todo!());
     todo!()
 }
 
@@ -7,77 +8,109 @@ pub fn count_xmas(word_serach: &str) -> u32 {
         .split("\n")
         .map(|s| s.chars().collect())
         .collect();
-    let rows = matrix.len();
-    let columns = matrix[0].len();
+    let row_count = matrix.len();
+    let column_count = matrix[0].len();
 
     let r = matrix
         .iter()
         .enumerate()
-        .map(|(i, cs)| {
-            return cs
-                .iter()
+        .map(|(row_index, cs)| {
+            cs.iter()
                 .enumerate()
-                .map(|(j, x)| {
-                    if (*x == X) {
-                        println!("found an X")
-                    }
-                    let row = if checkRow() { 1 } else { 0 };
-                    let diagonal = if checkDiagonal() { 1 } else { 0 };
-                    let column = if checkColumn() { 1 } else { 0 };
+                .map(|(column_index, _)| {
+                    let row = count_row(row_index, column_index, column_count, &matrix);
+                    let diagonal: u32 =
+                        count_diagonal(row_index, column_index, row_count, column_count, &matrix);
+                    let column: u32 = count_column(row_index, column_index, row_count, &matrix);
 
-                    return row + diagonal + column;
+                    row + diagonal + column
                 })
-                .sum();
+                .sum::<u32>()
         })
         .sum();
     return r;
 }
 
-fn checkRow(
+fn count_row(row: usize, column: usize, max_column: usize, matrix: &Vec<Vec<char>>) -> u32 {
+    let exists_forward = column + 3 < max_column
+        && matrix[row][column] == 'X'
+        && matrix[row][column + 1] == 'M'
+        && matrix[row][column + 2] == 'A'
+        && matrix[row][column + 3] == 'S';
+
+    let exists_back = column >= 3
+        && matrix[row][column] == 'X'
+        && matrix[row][column - 1] == 'M'
+        && matrix[row][column - 2] == 'A'
+        && matrix[row][column - 3] == 'S';
+
+    let forward: u32 = if exists_forward { 1 } else { 0 };
+    let back: u32 = if exists_back { 1 } else { 0 };
+    return forward + back;
+}
+
+fn count_diagonal(
     row: usize,
     column: usize,
     max_row: usize,
     max_column: usize,
-    letter: StartingLetter,
     matrix: &Vec<Vec<char>>,
-) -> bool {
-    // I can potentially do a matrix[i][j] == X.first() && matrix[i][j+1] == X.second() … ||
-    // matrix[i][j] == S.first() && matrix[i][j+1] == S.second() …
+) -> u32 {
+    let has_space_back = column >= 3;
+    let has_space_up = row >= 3;
+    let has_space_forward = column + 3 < max_column;
+    let has_space_down = row + 3 < max_row;
 
-    if column < max_column {
-        return matrix[row][column + 1] == letter.second();
-    }
-    return false;
+    let exists_back_up = has_space_back
+        && has_space_up
+        && matrix[row][column] == 'X'
+        && matrix[row - 1][column - 1] == 'M'
+        && matrix[row - 2][column - 2] == 'A'
+        && matrix[row - 3][column - 3] == 'S';
+    let exists_forward_up = has_space_forward
+        && has_space_up
+        && matrix[row][column] == 'X'
+        && matrix[row - 1][column + 1] == 'M'
+        && matrix[row - 2][column + 2] == 'A'
+        && matrix[row - 3][column + 3] == 'S';
+
+    let exists_back_down = has_space_back
+        && has_space_down
+        && matrix[row][column] == 'X'
+        && matrix[row + 1][column - 1] == 'M'
+        && matrix[row + 2][column - 2] == 'A'
+        && matrix[row + 3][column - 3] == 'S';
+
+    let exists_forward_down = has_space_forward
+        && has_space_down
+        && matrix[row][column] == 'X'
+        && matrix[row + 1][column + 1] == 'M'
+        && matrix[row + 2][column + 2] == 'A'
+        && matrix[row + 3][column + 3] == 'S';
+
+    let back_up = if exists_back_up { 1 } else { 0 };
+    let forward_up = if exists_forward_up { 1 } else { 0 };
+    let back_down = if exists_back_down { 1 } else { 0 };
+    let forward_down = if exists_forward_down { 1 } else { 0 };
+
+    return back_up + forward_up + back_down + forward_down;
 }
 
-static X: char = 'X';
+fn count_column(row: usize, column: usize, max_row: usize, matrix: &Vec<Vec<char>>) -> u32 {
+    let exists_up = row >= 3
+        && matrix[row][column] == 'X'
+        && matrix[row - 1][column] == 'M'
+        && matrix[row - 2][column] == 'A'
+        && matrix[row - 3][column] == 'S';
+    let exists_down = row + 3 < max_row
+        && matrix[row][column] == 'X'
+        && matrix[row + 1][column] == 'M'
+        && matrix[row + 2][column] == 'A'
+        && matrix[row + 3][column] == 'S';
 
-enum StartingLetter {
-    X,
-    S,
-}
-
-impl StartingLetter {
-    fn second(&self) -> char {
-        match *self {
-            StartingLetter::X => 'M',
-            StartingLetter::S => 'A',
-        }
-    }
-
-    fn third(&self) -> char {
-        match *self {
-            StartingLetter::X => 'A',
-            StartingLetter::S => 'M',
-        }
-    }
-
-    fn fourth(&self) -> char {
-        match *self {
-            StartingLetter::X => 'S',
-            StartingLetter::S => 'X',
-        }
-    }
+    let up = if exists_up { 1 } else { 0 };
+    let down = if exists_down { 1 } else { 0 };
+    return up + down;
 }
 
 #[cfg(test)]
