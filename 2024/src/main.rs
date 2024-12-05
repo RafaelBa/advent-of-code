f748115c593de08f65747e1b4baca9ec42d4ce29
Rafael <11751298+RafaelBa@users.noreply.github.com>
2024-12-05 15:59:13 +0100

    Add Day 4 to main; first step in refactoring main

--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -1,12 +1,18 @@
 use regex::Regex;
 use std::env;
 mod day_1;
+mod day_4;
+
+fn text(day: &str, part: &str) -> String {
+    return format!("Day {day}, puzzle {part}");
+}
 
 fn print_solution(day: &str, part: &str) {
     match (day, part) {
-        ("1", "1") =>     println!("Day 1, puzzle 1: {}", day_1::solve_1()),
+        ("1", "1") => println!("Day 1, puzzle 1: {}", day_1::solve_1()),
         ("1", "2") => println!("Day 1, puzzle 2: {}", day_1::solve_2()),
-        ("4", "1") => println!("here could be the solution for day 4, part 1"),
+        ("4", "1") => println!("{}: {}", text(day, part), day_4::solve_1()),
+        ("4", "2") => println!("{}: {}", text(day, part), day_4::solve_2()),
         (d, p) => println!("No solution for day {} part {}", d, p),
     }
 }
